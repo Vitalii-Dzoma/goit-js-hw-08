e7c5278333428e8f497e5e5efe78244e070ce971
Vitalii-Dzoma <vitoos900@gmail.com>
2021-11-05 18:04:46 +0200

    финал

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,37 +1,42 @@
 import { throttle } from "lodash"
-
 const mailEl = document.querySelector('[name="email"]')
 const messageEl = document.querySelector('[name="message"]')
 const btnEl = document.querySelector('button')
 const formEl = document.querySelector('.feedback-form')
+let parsedData;
 mailEl.addEventListener("input", throttle(writeIn, 500) )
 messageEl.addEventListener("input", throttle(writeIn, 500))
-let parsedData
-if (localStorage.getItem("feedback-form-state")) {
-  parsedData = JSON.parse(localStorage.getItem("feedback-form-state"))
-    mailEl.value = parsedData.email
-  messageEl.value = parsedData.message
-  
-}
 
 function writeIn() {
     localStorage.setItem("feedback-form-state", JSON.stringify({
         email: mailEl.value,
         message: messageEl.value,
+        
     }))
 
-    parsedData = JSON.parse(localStorage.getItem("feedback-form-state"))
-}
-
-
-formEl.addEventListener("submit", cleanLocalStorage)
+    
+parsedData = JSON.parse(localStorage.getItem("feedback-form-state"))
      
-function cleanLocalStorage(event) {
-    event.preventDefault()
-    console.log(parsedData)
-    formEl.reset()
-    localStorage.clear()
+
 }
+
+function cleanLocalStorage(event) {
+     console.log(parsedData)
+             event.preventDefault()
+             formEl.reset();
+                localStorage.clear();
+    }
+    
+    formEl.addEventListener("submit", cleanLocalStorage)
+function getResult() {
+    if (parsedData) {
+        mailEl.value = parsedData.email
+    messageEl.value = parsedData.message
+    }
+
+}
+
+
   
 
 
